355dc54d2c93012a8023f0e3671127b831377f86
Stefan Georgieff <stefan@sg6.eu>
2018-01-15 21:43:26 +0100

    Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  income: Number = 0;
+  expenses: Number = 0;
+  advance_tax: Number = 0;
+  advance_ins: Number = 0;
 }

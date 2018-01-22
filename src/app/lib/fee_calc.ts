2a91a6127b2aeac4de44c3500255ca9d1b529988
Stefan <stefan@sg6.eu>
2018-01-22 22:05:19 +0100

    est calc formula

--- src/app/lib/fee_calc.ts
+++ src/app/lib/fee_calc.ts
@@ -14,6 +14,37 @@ export class FeeCalc {
     }
 
     estCalc(income) {
-       return income * 0.2 
+        let tax: number = 0;
+
+        const BASE_TAX = [
+            { min:      0, max: 11000,   rate: 0 },
+            { min:  11000, max: 18000,   rate: 0.25 },
+            { min:  18000, max: 31000,   rate: 0.35 },
+            { min:  31000, max: 60000,   rate: 0.42 },
+            { min:  60000, max: 90000,   rate: 0.48 },
+            { min:  90000, max: 100000,  rate: 0.50 },
+            { min: 100000, max: Infinity,rate: 0.55 },
+        ]
+
+        let i = 0;
+        for (let base of BASE_TAX) {
+            let range = 0;
+            if (base.max !== Infinity) {
+                range = base.max - base.min
+            }
+            if (range === 0 || income <= 0) return tax
+
+            let base_sum: number = 0
+            if(base.max !== Infinity) {
+                base_sum = Math.min(income, range)
+            }
+            
+            income -= base_sum
+            tax += base_sum * base.rate
+
+            console.log(base.rate, base_sum, tax, income, range)
+        }
+
+        return tax 
     }
 }

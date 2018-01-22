516d5081f606b06a0517cbe8a2aeb4fc0327096f
Stefan <stefan@sg6.eu>
2018-01-22 19:00:11 +0100

    SVA calc added

--- src/app/lib/sva_calc.ts
+++ src/app/lib/sva_calc.ts
@@ -1,8 +1,9 @@
 export class SvaCalc {
     
-    /// MOCK DATA !!!
     /// calculcations to be done
     calc(income) {
-        return income * 0.3;
+        // 27,68% = 18,5% Pensions- + 7,65% Krankenversicherung +  1,53% Vorsorge
+        // 9,60 ist der monatliche Unfallversicherungsbeitrag
+        return (income * 0.2768) + (9.6 * 12);
     }
 }

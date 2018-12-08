1f390bfed81509f05386c81771e479e78136be31
Stefan Georgieff <stefan@sg6.eu>
2018-12-08 23:57:43 +0100

    allowance lower tax base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,9 +28,11 @@ export class AppComponent implements OnInit {
 
   sva_sum: number = 0;
   profit: number = 0;
+  base: number = 0;
 
   additional_allowance: number = 0;
   allowance: number = 0;
+  flat: boolean = false;
 
   readonly MAX_ALLOWANCE = 3900;
   readonly ALLOWANCE_RATE = 0.13;
@@ -44,17 +46,21 @@ export class AppComponent implements OnInit {
   }
 
   calcSum() {
-    this.ebitsva = this.income - this.expenses;
-    this.sva_sum = this._calc.svaCalc(this.ebitsva);
-    this.ebit = this.ebitsva - this.sva_sum;
-    this.advance_pamyments = this.advance_ins + this.advance_tax;
+    this.ebit = this.income - (this.expenses + this.advance_ins)
+    this.advance_pamyments = this.advance_ins + this.advance_tax
 
-    this.allowance = Math.min(this.MAX_ALLOWANCE, this.ALLOWANCE_RATE * this.ebit) + this.additional_allowance;
-    this.est = this._calc.estCalc(this.ebit - this.allowance);
+    this.allowance = Math.min(this.MAX_ALLOWANCE, this.ALLOWANCE_RATE * this.ebit) + this.additional_allowance
+    if (this.flat) {
+      this.allowance += this.income * 0.12
+    }
+    this.base = this.ebit - this.allowance
+
+    this.est = this._calc.estCalc(this.base);
+    this.sva_sum = this._calc.svaCalc(this.base + Math.min(this.advance_ins, this._calc.svaCalc(this.base)));
 
     this.full_payments = this.est + this.sva_sum;
     this.open_payments = this.full_payments - this.advance_pamyments;
 
-    this.profit = this.ebit - this.est;
+    this.profit = this.ebit - this.est - this.sva_sum + this.advance_ins;
   }
 }

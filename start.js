9b0951b574956cb10e9a63aea958f125c371e700
Stefan Georgieff <stefan@sg6.eu>
2019-09-09 18:07:51 +0200

    prod config

--- start.js
+++
@@ -1,5 +0,0 @@
-const connect = require('connect');
-const serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + "/dist")).listen(3010, function(){
-    console.log('Server running on 3010...');
-});
